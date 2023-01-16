79cfb52190c5d02abb3444668c6d1a412cae9c3e
adeshpokhrel15 <adeshpokhrel15@gmail.com>
2023-01-16 17:47:54 +0545

    on boarding screen

---
+++ lib/utils/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:two_wheelers/views/onboarding_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnboardingScreen(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:two_wheelers/main.dart';
+import 'package:two_wheelers/utils/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
